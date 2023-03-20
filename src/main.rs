e0ca92bc7b62181803dc6a56ce1ae6ae7c6b33ba
Alex Lyzhoft <alexlyzhoft@twinsbaseball.com>
2023-03-19 21:38:55 -0500

    feat: 3.5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listerner = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    run(listerner)?.await
 }
